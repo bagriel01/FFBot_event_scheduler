a571d290044fcc0b7e78b99798a126ac2b7fc2a4
Gabriel Rosa <62349836+bagriel01@users.noreply.github.com>
2026-04-12 18:29:56 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,16 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 APPROVED_EVENT_CHANNEL_ID = os.getenv("APPROVED_EVENT_CHANNEL_ID")
+
 ADMIN_USER_IDS = [
     int(user_id.strip())
     for user_id in os.getenv("ADMIN_USER_IDS", "").split(",")
     if user_id.strip().isdigit()
 ]
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = int(os.getenv("PORT", 10000))
