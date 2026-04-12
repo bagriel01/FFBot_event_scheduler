990121a1ab7116d2bedfaf3b78ac97e63add0802
Gabriel Rosa <62349836+bagriel01@users.noreply.github.com>
2026-04-12 19:19:29 -0300

    Update main.py

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -35,13 +35,12 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Use /Ping to check if the bot is working."
     )
 
+
 async def main():
     if not BOT_TOKEN:
         raise RuntimeError("BOT_TOKEN is not set in the environment variables.")
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    # Handlers
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler(["Ping_at", "Ping"], ping))
     app.add_handler(CommandHandler("help", help_command))
@@ -49,22 +48,21 @@ async def main():
     app.add_handler(build_ffpost_handler())
     app.add_handler(build_approval_handler())
 
-    await app.initialize()
-    await app.start()
-
     webhook_path = f"/webhook/{BOT_TOKEN}"
     webhook_url = f"{WEBHOOK_URL}{webhook_path}"
+
+    await app.bot.delete_webhook(drop_pending_updates=True)
 
     await app.bot.set_webhook(webhook_url)
 
     logger.info(f"Webhook set to: {webhook_url}")
-
 
     await app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
         url_path=webhook_path,
     )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
